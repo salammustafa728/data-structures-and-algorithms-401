4caca6abe21dc62668bd0f45167601be58f8e3da
salammustafa728 <salammustafa728@gmail.com>
2022-03-05 20:44:43 +0200

    solve chal#5

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,21 @@
+# Singly Linked List
+
+Singly linked list refers to the number of references the node has.
+
+## Challenge
+
+This is a linked list challenge that defind a linked list class that have an insert to insert in LL method, append to append in LL and includs to look for specific value in LL also toString method that print the LL values in specific format.
+
+
+## Approach & Efficiency
+
+I used the big o(1) and o(n) in the challange
+
+## API
+
+In The LL there's a 4 methods:
+
+* insert to insert in LL method
+* append to append in LL
+* includs to look for specific value in LL
+* toString method that print the LL values in specific format.
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,57 @@
-'use strict';
+"use strict";
+const Node = require("./node");
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      //ll is empty
+      this.head = node;
+    } else {
+      //ll is not empty
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+  includs(value) {
+    if (this.head !== null) {
+      let current = this.head;
+      while (current !== null) {
+        if (current.value !== value) {
+          current = current.next;
+        } else return true;
+      }
+    } else return false;
+  }
+  toString() {
+    let strLL = '';
+    if (this.head !== null) {
+      let current = this.head;
+      while (current !== null) {
+        // console.log(current.value);
+        strLL += `{ ${current.value} } -> `;
+        current = current.next;
+      }
+      strLL+=`NULL`;
+      return strLL;
+    } else return 'NULL';
+  }
 }
 
 module.exports = LinkedList;
