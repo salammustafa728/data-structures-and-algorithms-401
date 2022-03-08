7b63003f660eb821eba06c1c7020219216a610ed
salammustafa728 <salammustafa728@gmail.com>
2022-03-08 22:40:36 +0200

    solve challenge#7

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -15,42 +15,84 @@ class LinkedList {
       this.head = node;
     }
   }
-  append(value) {
-    const node = new Node(value);
-    if (!this.head) {
-      //ll is empty
-      this.head = node;
-    } else {
-      //ll is not empty
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = node;
+  // inserBefore(val,newVal){
+  //   const node = new Node(newVal);
+  //   let current = this.head;
+  //   while(current.next.value!==val){
+  //     current=current.next;
+  //   }
+  //   node.next = current.next;
+  //   current.next = node;
+  // }
+  // inserAfter(val,newVal){
+  //   const node = new Node(newVal);
+  //   let current = this.head;
+  //   while(current.value!==val){
+  //     current=current.next;
+  //   }
+  //   node.next = current.next;
+  //   current.next = node;
+  // }
+  // append(value) {
+  //   const node = new Node(value);
+  //   if (!this.head) {
+  //     //ll is empty
+  //     this.head = node;
+  //   } else {
+  //     //ll is not empty
+  //     let current = this.head;
+  //     while (current.next) {
+  //       current = current.next;
+  //     }
+  //     current.next = node;
+  //   }
+  // }
+  // includs(value) {
+  //   if (this.head !== null) {
+  //     let current = this.head;
+  //     while (current !== null) {
+  //       if (current.value !== value) {
+  //         current = current.next;
+  //       } else return true;
+  //     }
+  //   } else return false;
+  // }
+  // toString() {
+  //   let strLL = '';
+  //   if (this.head !== null) {
+  //     let current = this.head;
+  //     while (current !== null) {
+  //       // console.log(current.value);
+  //       strLL += `{ ${current.value} } -> `;
+  //       current = current.next;
+  //     }
+  //     strLL += `NULL`;
+  //     return strLL;
+  //   } else return 'NULL';
+  // }
+
+  kthFromEnd(k) {
+    // to count the length of ll
+    let len = 0;
+    let current = this.head;
+    while (current !== null) {
+      current = current.next;
+      len++;
     }
-  }
-  includs(value) {
-    if (this.head !== null) {
-      let current = this.head;
-      while (current !== null) {
-        if (current.value !== value) {
-          current = current.next;
-        } else return true;
+    if (k < 0) {
+      return 'Exception';
+    }
+    if (len < k) {
+      return 'Exception';
+    }
+    current = this.head;
+    for (let i = 0; i < len - k - 1; i++) {
+      if(current.next===null){
+        return;
       }
-    } else return false;
-  }
-  toString() {
-    let strLL = '';
-    if (this.head !== null) {
-      let current = this.head;
-      while (current !== null) {
-        // console.log(current.value);
-        strLL += `{ ${current.value} } -> `;
-        current = current.next;
-      }
-      strLL += `NULL`;
-      return strLL;
-    } else return 'NULL';
+      current = current.next;
+    }
+    return current.value;
   }
 
 }
