644a7534ce4970f3319d855c1d2ed71d6e50d61c
salammustafa728 <salammustafa728@gmail.com>
2022-05-06 09:14:15 +0300

    edit mergesort

--- javascript/sort/Merge-Sort/mergeSort.js
+++ javascript/sort/Merge-Sort/mergeSort.js
@@ -5,31 +5,56 @@ const mergeSort = (arr) => {
 
   if (len === 1) {
     return arr;
-  }else{
+  } else {
     let mid = Math.floor(len / 2);
     let arrLeft = arr.slice(0, mid);
     let arrRigth = arr.slice(mid);
 
-    return mergeArrays(mergeSort(arrLeft), mergeSort(arrRigth));
+    mergeSort(arrLeft);
+    mergeSort(arrRigth);
+    mergeArrays(arrLeft,arrRigth,arr);
   }
-
+  return arr;
 };
 
-const mergeArrays = (left, rigth) => {
-  let finalArr = [];
+const mergeArrays = (left, rigth,arr) => {
+  // let finalArr = [];
   let idxLeft = 0;
   let idxRigth = 0;
+  let i = 0;
 
   while (idxLeft < left.length && idxRigth < rigth.length) {
-    if (left[idxLeft] < rigth[idxRigth]) {
-      finalArr.push(left[idxLeft]);
+    if (left[idxLeft] <= rigth[idxRigth]) {
+      arr[i] = left[idxLeft];
       idxLeft++;
     } else {
-      finalArr.push(rigth[idxRigth]);
+      arr[i] = rigth[idxRigth];
       idxRigth++;
     }
+    i++;
+    // if (left[idxLeft] < rigth[idxRigth]) {
+    //   arr.push(left[idxLeft]);
+    //   idxLeft++;
+    // } else {
+    //   arr.push(rigth[idxRigth]);
+    //   idxRigth++;
+    // }
   }
-  return finalArr.concat(left.slice(idxLeft)).concat(rigth.slice(idxRigth));
+  if (idxLeft === left.length) {
+    while (i < arr.length) {
+      arr[i] = rigth[idxRigth];
+      idxRigth++;
+      i++;
+    }
+  } else {
+    while (i < arr.length) {
+      arr[i] = left[idxLeft];
+      idxLeft++;
+      i++;
+    }
+  }
+
+  // return finalArr.concat(left.slice(idxLeft)).concat(rigth.slice(idxRigth));
 };
 
 module.exports = mergeSort;
