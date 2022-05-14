c586dcbf399a105869278c895d38e08c50d1cad6
salammustafa728 <salammustafa728@gmail.com>
2022-05-14 10:08:29 +0300

    hashtable

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -33,43 +33,43 @@ class LinkedList {
   //   node.next = current.next;
   //   current.next = node;
   // }
-  // append(value) {
-  //   const node = new Node(value);
-  //   if (!this.head) {
-  //     //ll is empty
-  //     this.head = node;
-  //   } else {
-  //     //ll is not empty
-  //     let current = this.head;
-  //     while (current.next) {
-  //       current = current.next;
-  //     }
-  //     current.next = node;
-  //   }
-  // }
-  // includs(value) {
-  //   if (this.head !== null) {
-  //     let current = this.head;
-  //     while (current !== null) {
-  //       if (current.value !== value) {
-  //         current = current.next;
-  //       } else return true;
-  //     }
-  //   } else return false;
-  // }
-  // toString() {
-  //   let strLL = '';
-  //   if (this.head !== null) {
-  //     let current = this.head;
-  //     while (current !== null) {
-  //       // console.log(current.value);
-  //       strLL += `{ ${current.value} } -> `;
-  //       current = current.next;
-  //     }
-  //     strLL += `NULL`;
-  //     return strLL;
-  //   } else return 'NULL';
-  // }
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      //ll is empty
+      this.head = node;
+    } else {
+      //ll is not empty
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+  includs(value) {
+    if (this.head !== null) {
+      let current = this.head;
+      while (current !== null) {
+        if (current.value !== value) {
+          current = current.next;
+        } else return true;
+      }
+    } else return false;
+  }
+  toString() {
+    let strLL = '';
+    if (this.head !== null) {
+      let current = this.head;
+      while (current !== null) {
+        // console.log(current.value);
+        strLL += `{ ${current.value} } -> `;
+        current = current.next;
+      }
+      strLL += `NULL`;
+      return strLL;
+    } else return 'NULL';
+  }
 
   // kthFromEnd(k) {
   //   // to count the length of ll
